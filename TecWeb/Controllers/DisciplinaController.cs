80955d6779f3036998bb517ff664cd1f11975ef8
Alex Lopes <alexlr10@hotmail.com>
2020-04-30 16:48:35 -0300

    Passando parametros

--- TecWeb/Controllers/DisciplinaController.cs
+++ TecWeb/Controllers/DisciplinaController.cs
@@ -5,18 +5,32 @@ using System.Web;
 using System.Web.Mvc;
 using TecWeb.Models;
 
-namespace TecWeb.Controllers
-{
-    public class DisciplinaController : Controller
-    {
+namespace TecWeb.Controllers {
+    public class DisciplinaController : Controller {
         // GET: Disciplina
-        public ActionResult Index()
-        {
+        public ActionResult Index(int idAluno, string nomeAluno) {
             List<Disciplina> disciplina = new List<Disciplina>();
-               disciplina.Add( new Disciplina(1, "Tecnologia Web", "5A", "SI"));
-               disciplina.Add( new Disciplina(1, "Banco de Dados 1 ", "5A", "SI"));
-               disciplina.Add( new Disciplina(1, "Banco de Dados 2", "1A", "SI"));
-               disciplina.Add( new Disciplina(1, "Arquitetura de Computadores", "1A", "SI"));
+
+            ViewBag.idAluno = idAluno;
+            ViewBag.nomeAluno = nomeAluno;
+
+            disciplina.Add(new Disciplina(1, 1, "Tecnologia Web", "5A", "SI"));
+            disciplina.Add(new Disciplina(1, 2, "Banco de Dados 1 ", "5A", "SI"));
+            disciplina.Add(new Disciplina(1, 3, "Banco de Dados 2", "1A", "SI"));
+
+            disciplina.Add(new Disciplina(2, 1, "Arquitetura de Computadores", "1A", "SI"));
+            disciplina.Add(new Disciplina(2, 2, "Arquitetura de Computadores", "1A", "SI"));
+            disciplina.Add(new Disciplina(2, 3, "Arquitetura de Computadores", "1A", "SI"));
+            disciplina.Add(new Disciplina(2, 4, "Arquitetura de Computadores", "1A", "SI"));
+
+            disciplina.Add(new Disciplina(3, 5, "Arquitetura de Computadores", "1A", "SI"));
+            disciplina.Add(new Disciplina(3, 6, "Arquitetura de Computadores", "1A", "SI"));
+
+            disciplina.Add(new Disciplina(4, 5, "Arquitetura de Computadores", "1A", "SI"));
+            disciplina.Add(new Disciplina(4, 6, "Arquitetura de Computadores", "1A", "SI"));
+
+
+
 
 
 
--- TecWeb/Models/Disciplina.cs
+++ TecWeb/Models/Disciplina.cs
@@ -6,12 +6,14 @@ using System.Web;
 namespace TecWeb.Models {
     public class Disciplina {
         public int IdDisciplina { get; set; }
+        public int IdAluno { get; set; }
         public string Nome { get; set; }
         public string Semestre { get; set; }
         public string Curso { get; set; }
 
-        public Disciplina(int idDisciplina, string nome, string semestre, string curso) {
+        public Disciplina(int idDisciplina, int idAluno, string nome, string semestre, string curso) {
             IdDisciplina = idDisciplina;
+            IdAluno = idAluno;
             Nome = nome;
             Semestre = semestre;
             Curso = curso;
