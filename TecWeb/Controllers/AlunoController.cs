4b70ed039a49d1576cd9e1bae7900f073ce27cd6
Alex Lopes <alexlr10@hotmail.com>
2020-05-01 15:29:02 -0300

    conexao com bd

--- TecWeb/Controllers/AlunoController.cs
+++ TecWeb/Controllers/AlunoController.cs
@@ -1,29 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TecWeb.Models;
 
-namespace TecWeb.Controllers
-{
-    public class AlunoController : Controller
-    {
+namespace TecWeb.Controllers {
+    public class AlunoController : Controller {
         // GET: Aluno
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
+            SqlConnection minhaConexao = new SqlConnection(ConfigurationManager.ConnectionStrings["minhaConexao"].ConnectionString);
+            minhaConexao.Open();
+
+            string select = "SELECT * FROM Aluno";
+            SqlCommand selectCommand = new SqlCommand(select, minhaConexao);
+            SqlDataReader sqlRead = selectCommand.ExecuteReader();
+
+
             List<Aluno> Alunos = new List<Aluno>();
-            Alunos.Add(new Aluno(1, "Manuel", 123456, Convert.ToDateTime("12/12/2015")));
-            Alunos.Add(new Aluno(2, "Pedro", 14234123, Convert.ToDateTime("12/12/2010")));
-            Alunos.Add(new Aluno(3, "Miguel", 51234123, Convert.ToDateTime("12/12/2008")));
-            Alunos.Add(new Aluno(4, "Lucas", 12341234, Convert.ToDateTime("12/12/1990")));
-            Alunos.Add(new Aluno(5, "Vitor", 543545, Convert.ToDateTime("12/12/2020")));
-            Alunos.Add(new Aluno(6, "Rafael", 1235143, Convert.ToDateTime("12/12/2005")));
-           
-            
-            
-            
-            
+
+            while (sqlRead.Read()) {
+                Alunos.Add(new Aluno(int.Parse(sqlRead["IdAluno"].ToString()),
+                                     sqlRead["Nome"].ToString(),
+                                     int.Parse(sqlRead["RA"].ToString()),
+                                     Convert.ToDateTime(sqlRead["DataNascimento"].ToString())));
+            }
+
+            //Alunos.Add(new Aluno(1, "Manuel", 123456, Convert.ToDateTime("12/12/2015")));
+            //Alunos.Add(new Aluno(2, "Pedro", 14234123, Convert.ToDateTime("12/12/2010")));
+            //Alunos.Add(new Aluno(3, "Miguel", 51234123, Convert.ToDateTime("12/12/2008")));
+            //Alunos.Add(new Aluno(4, "Lucas", 12341234, Convert.ToDateTime("12/12/1990")));
+            //Alunos.Add(new Aluno(5, "Vitor", 543545, Convert.ToDateTime("12/12/2020")));
+            //Alunos.Add(new Aluno(6, "Rafael", 1235143, Convert.ToDateTime("12/12/2005")));
+
+
+
+
+
             return View(Alunos);
         }
     }
--- TecWeb/Controllers/DisciplinaController.cs
+++ TecWeb/Controllers/DisciplinaController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,34 +11,50 @@ namespace TecWeb.Controllers {
     public class DisciplinaController : Controller {
         // GET: Disciplina
         public ActionResult Index(int idAluno, string nomeAluno) {
-            List<Disciplina> disciplina = new List<Disciplina>();
+            //List<Disciplina> disciplina = new List<Disciplina>();
 
             ViewBag.idAluno = idAluno;
             ViewBag.nomeAluno = nomeAluno;
 
-            disciplina.Add(new Disciplina(1, 1, "Tecnologia Web", "5A", "SI"));
-            disciplina.Add(new Disciplina(1, 2, "Banco de Dados 1 ", "5A", "SI"));
-            disciplina.Add(new Disciplina(1, 3, "Banco de Dados 2", "1A", "SI"));
+            SqlConnection minhaConexao = new SqlConnection(ConfigurationManager.ConnectionStrings["minhaConexao"].ConnectionString);
+            minhaConexao.Open();
 
-            disciplina.Add(new Disciplina(2, 1, "Arquitetura de Computadores", "1A", "SI"));
-            disciplina.Add(new Disciplina(2, 2, "Arquitetura de Computadores", "1A", "SI"));
-            disciplina.Add(new Disciplina(2, 3, "Arquitetura de Computadores", "1A", "SI"));
-            disciplina.Add(new Disciplina(2, 4, "Arquitetura de Computadores", "1A", "SI"));
-
-            disciplina.Add(new Disciplina(3, 5, "Arquitetura de Computadores", "1A", "SI"));
-            disciplina.Add(new Disciplina(3, 6, "Arquitetura de Computadores", "1A", "SI"));
-
-            disciplina.Add(new Disciplina(4, 5, "Arquitetura de Computadores", "1A", "SI"));
-            disciplina.Add(new Disciplina(4, 6, "Arquitetura de Computadores", "1A", "SI"));
-
-            List<Disciplina> DisciplinasDeAluno = disciplina.Where(x => x.IdAluno == idAluno).ToList();
+            string select = "SELECT * from Disciplina inner join AlunoDisciplina " +
+                "on Disciplina.IdDisciplina = AlunoDisciplina.IdDisciplina where AlunoDisciplina.IdAluno = " + idAluno;
+            SqlCommand selectCommand = new SqlCommand(select, minhaConexao);
+            SqlDataReader sqlRead = selectCommand.ExecuteReader();
 
 
+            List<Disciplina> disciplina = new List<Disciplina>();
+
+            while (sqlRead.Read()) {
+                disciplina.Add(new Disciplina(int.Parse(sqlRead["IdDisciplina"].ToString()),
+                                     idAluno,
+                                     sqlRead["Nome"].ToString(),
+                                     sqlRead["Semestre"].ToString(),
+                                     sqlRead["Curso"].ToString()));
+            }
 
 
+            //disciplina.Add(new Disciplina(1, 1, "Tecnologia Web", "5A", "SI"));
+            //disciplina.Add(new Disciplina(1, 2, "Banco de Dados 1 ", "5A", "SI"));
+            //disciplina.Add(new Disciplina(1, 3, "Banco de Dados 2", "1A", "SI"));
+
+            //disciplina.Add(new Disciplina(2, 1, "Arquitetura de Computadores", "1A", "SI"));
+            //disciplina.Add(new Disciplina(2, 2, "Arquitetura de Computadores", "1A", "SI"));
+            //disciplina.Add(new Disciplina(2, 3, "Arquitetura de Computadores", "1A", "SI"));
+            //disciplina.Add(new Disciplina(2, 4, "Arquitetura de Computadores", "1A", "SI"));
+
+            //disciplina.Add(new Disciplina(3, 5, "Arquitetura de Computadores", "1A", "SI"));
+            //disciplina.Add(new Disciplina(3, 6, "Arquitetura de Computadores", "1A", "SI"));
+
+            //disciplina.Add(new Disciplina(4, 5, "Arquitetura de Computadores", "1A", "SI"));
+            //disciplina.Add(new Disciplina(4, 6, "Arquitetura de Computadores", "1A", "SI"));
+
+            //List<Disciplina> DisciplinasDeAluno = disciplina.Where(x => x.IdAluno == idAluno).ToList();
 
 
-            return View(DisciplinasDeAluno);
+            return View(disciplina);
         }
     }
 }
